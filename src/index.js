6e78d390f729a23095b791508b3c18ae2a0f616d
IKeeny <53686618+IKeeny@users.noreply.github.com>
2020-12-16 12:37:30 +0800

    first commit million-anwser

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Button} from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button>Start</Button>
   </React.StrictMode>,
   document.getElementById('root')
 );
